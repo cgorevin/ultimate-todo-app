2dce99f3a53470a9bfbd4b09cc668120ea54da89
Cait Gorevin <cgorevin@adwerx.com>
2020-12-15 11:19:23 -0500

    15122020: Update test to red for Github Actions
    Update test to failing to test Github Actions run-test-suite.

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,5 +1,12 @@
 # typed: strict
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :request do
+  context 'when POSTing a new task' do
+    it 'will return a "HELLO" message' do
+      expect(1 + 1).to eq(3)
+    end
+  end
 end
