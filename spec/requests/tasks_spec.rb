86ad694a080a8dbe7bda0b94fd6d6bfbd06570f1
Cait Gorevin <cgorevin@adwerx.com>
2020-12-15 11:26:13 -0500

    15122020: Update test to green
    Update failing test to green to confirm functionality of Github Action
    run-test-suite workflow.

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -6,7 +6,7 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :request do
   context 'when POSTing a new task' do
     it 'will return a "HELLO" message' do
-      expect(1 + 1).to eq(3)
+      expect(1 + 1).to eq(2)
     end
   end
 end
