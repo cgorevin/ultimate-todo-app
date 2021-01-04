26db66b26ce740d63db88ce8ca020d46867a57df
Cait Gorevin <cgorevin@adwerx.com>
2021-01-04 18:32:05 -0500

    6: Run git fetch prior to pronto run
    In the Pronto workflow, run git fetch prior to the pronto run
    command to correct Rugged::ReferenceError.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,6 @@
 const { environment } = require('@rails/webpacker');
 const dotenv = require("dotenv");
 
-dotenv.config({ path: ".env", silent: true }));
+dotenv.config({ path: ".env", silent: true });
 
 module.exports = environment;
